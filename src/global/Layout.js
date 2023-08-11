c054e6c1f2bf8fff5c2aedeff2647357da5ab9a9
Bogumił <hello@milewski.me>
2023-08-11 17:45:33 +0200

    removed unecessary Script

--- src/global/Layout.js
+++ src/global/Layout.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import GlobalStyle from "../styles/GlobalStyle";
 import Nav from "../components/organisms/Nav";
 import Footer from "../components/organisms/Footer";
-import { Script } from "gatsby";
 
 const Layout = ({ children }) => {
   const handleTabKey = (e) => {
@@ -29,14 +28,6 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <Footer />
-      <Script
-        key="lekarzebezkolejki-script"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(doc,id){if(!doc.getElementById(id)){var scr,pscr=doc.getElementsByTagName("script")[0];scr=doc.createElement("script");scr.id=id;scr.type='text/javascript';scr.src="https://lekarzebezkolejki.pl/ron-www/resources/javascript/widgets/LBKWidgets.js";pscr.parentNode.insertBefore(scr, pscr);}}(document,"osoz-widget-script");
-          `,
-        }}
-      />,
     </>
   );
 }
