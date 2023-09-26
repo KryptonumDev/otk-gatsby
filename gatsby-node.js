1f82c5049378a446f763c56fc73d919d1ae5976f
kryptonumstudio <kryptonumstudio@gmail.com>
2023-09-26 13:16:21 +0200

    rework redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,14 @@
 const redirects = require("./redirects.json")
+const fs = require('fs')
 
-exports.createPages = async ({ actions }) => {
-  const { createRedirect } = actions;
-
-  redirects.forEach(redirect => {
-    createRedirect({
-      fromPath: redirect.fromPath,
-      toPath: redirect.toPath,
-      isPermanent: redirect.isPermanent || false
-    })
-  })
-}
+exports.onPostBuild = async () => {  
+    const redirectConfig = redirects.map(redirect => (
+      `[[redirects]]
+        from = "${redirect.from}"
+        to = "${redirect.to}"
+        status = ${redirect.isPermanent ? 301 : 302}
+        force = ${redirect.force || false}`
+    ));
+  
+    fs.writeFileSync('netlify.toml', redirectConfig.join('\n'));
+  };
