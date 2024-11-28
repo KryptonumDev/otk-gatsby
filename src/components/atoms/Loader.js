0bb2a26f3d71078c58f7a9613dbf08d0b1ce7643
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-11-28 14:20:53 +0100

    add aria-label for Loader element

--- src/components/atoms/Loader.js
+++ src/components/atoms/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Loader = () => (
-  <Wrapper></Wrapper>
+  <Wrapper aria-label="Ładowanie..."></Wrapper>
 )
 
 const Wrapper = styled.div`
